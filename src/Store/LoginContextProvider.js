cc513129d6d1edb881c209e7ba2db0e91c25d7a2
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-05-31 18:49:59 +0530

    auto logout after some time

--- src/Store/LoginContextProvider.js
+++ src/Store/LoginContextProvider.js
@@ -1,24 +1,26 @@
 import React, { useState } from "react";
 import { LoginToken } from "./LoginContext";
-
 const LoginContextProvider = (props) => {
-   const tokenFromLocalStorage =localStorage.getItem('token');
+  const tokenFromLocalStorage = localStorage.getItem("token");
   const [Token, setToken] = useState(tokenFromLocalStorage);
+  setTimeout(() => {
+    localStorage.removeItem("token");
+    setToken(null)
+  }, 50000);
 
   const isLoggedIn = (token) => {
-    localStorage.setItem('token',token)
+    localStorage.setItem("token", token);
     setToken(token);
-
   };
-  const LoggedOut=()=>{
-    localStorage.removeItem('token');
-    setToken(null)
-  }
+  const LoggedOut = () => {
+    localStorage.removeItem("token");
+   
+    setToken(null);
+  };
   const obj = {
     token: Token,
     isLoggedIn: isLoggedIn,
-    LoggedOut:LoggedOut
-   
+    LoggedOut: LoggedOut,
   };
   return (
     <LoginToken.Provider value={obj}>{props.children}</LoginToken.Provider>
